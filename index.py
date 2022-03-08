b7bedba22901fa20a610fb1f2705340735228b3b
shailendra7k <shailendrakumarssk@gmail.com>
2022-03-08 21:21:10 +0200

    graph added in index.py

--- index.py
+++ index.py
@@ -2,12 +2,21 @@
 import sys
 import subprocess
 import os
+import cgi
+import cgitb; cgitb.enable()
+import matplotlib
+import pylab
+import matplotlib
+matplotlib.use('Agg')
 
+os.environ['HOME'] = '/tmp'
+
+import matplotlib.pyplot as plt
 
 print("Content-type: text/html\n\n")
-print("<html>\n<body>")
+print("<html>\n<head><script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js'></script></head>\n<body><canvas id='bar-chart' width=300' height='150'></canvas>")
 print("<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">")
-
+os.environ[ 'HOME' ] = '/tmp/'
 mailq_status,mailq = subprocess.getstatusoutput('mailq | grep -c "^[A-F0-9]"')
 if int(mailq) >= 0:
 	print(f"Total Mail Queue: {mailq}\n")
@@ -31,5 +40,6 @@ if int(received_status) == 0:
 else:
 	print("Enable to fetch received Mails")
 
+print("""<script> new Chart(document.getElementById('bar-chart'), { type: 'bar', data: { labels: ['Mail Queue', 'Sent Mails', 'Received Mails'], datasets: [ { label: 'Number of Mails', backgroundColor: ['red', 'blue','yellow'], data: ["""+ f"""{mailq}"""+""","""+ f"""{sent_mails}"""+""","""+ f"""{received_mails}"""+"""] } ] },  options: {     legend: { display: false },   title: {    display: true,  text: 'Mail Graph'  }, scales: { yAxes: [{  ticks: { beginAtZero:true              }            }]        }    }});    </script>""")
 print("</div>\n</body>\n</html>")
 
